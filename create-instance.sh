73c1cfc4a0799c7bdfc0c008a6bb6192d8102548
Andreas Guther <andreas@guther.family>
2023-07-30 09:20:32 +0200

    feat: updated multipass command to match current version

--- create-instance.sh
+++ create-instance.sh
@@ -8,7 +8,7 @@ multipass delete k3s
 multipass purge
 
 # create and start virtual machine
-multipass launch --name k3s --cpus 2 --mem 2G --disk 5G
+multipass launch --name k3s --cpus 2 --memory 2G --disk 5G
 
 # restore directory
 popd
